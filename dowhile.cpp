aa6649dfd3ca33b84fa5e54621245cebedea4fa1
shubham-nov06 <dograshubham06@gmail.com>
2025-11-08 11:59:09 +0530

    Add file dowhile.cpp

---
+++ dowhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int fact = 1;
+    int i = 5;
+    do
+    {
+        fact = fact * i;
+        i--;
+    } while (i >= 1);
+    {
+        cout << "Fact is = " << fact;
+    }
+}
